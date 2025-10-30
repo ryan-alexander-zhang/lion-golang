19122b761aa8d24aabf3d03aff8b85de1799db97
ronghuanz <ronghuanz@saharalabs.ai>
2025-10-30 17:25:53 +0800

    feat: Enhance logging and tracing middleware

--- gin/main.go
+++ gin/main.go
@@ -47,78 +47,9 @@ func main() {
 	r.Use(requestid.New())
 
 	// Middleware to add logger with request ID, trace and span id field to context
-	r.Use(func(c *gin.Context) {
-		// Add request ID to logger
-		reqID := requestid.Get(c)
-		appLogger = appLogger.With(zap.String("request_id", reqID))
-
-		// Add trace_id and span_id to logger
-		sc := trace.SpanFromContext(c.Request.Context()).SpanContext()
-		if sc.IsValid() {
-			appLogger = appLogger.With(
-				zap.String("trace_id", sc.TraceID().String()),
-				zap.String("span_id", sc.SpanID().String()),
-			)
-		}
-		c.Set("logger", appLogger.With(zap.String("request_id", reqID)))
-		c.Next()
-	})
-
-	// Middleware to set tracing headers in the response
-	// - X-Request-Id
-	// - traceparent
-	// - Server-Timing
-	r.Use(func(c *gin.Context) {
-		// X-Request-ID
-		if rid := requestid.Get(c); rid != "" {
-			c.Header("X-Request-Id", rid)
-		}
-
-		// traceparent + Server-Timing
-		sc := trace.SpanFromContext(c.Request.Context()).SpanContext()
-		if sc.IsValid() {
-			// W3C traceparent
-			tp := "00-" + sc.TraceID().String() + "-" + sc.SpanID().String() + "-01"
-			c.Header("traceparent", tp)
-			c.Header("Server-Timing", `traceparent;desc="`+tp+`"`)
-		}
-		c.Next()
-	})
-
-	// Add a ginzap middleware, which:
-	//   - Logs all requests, like a combined access and error log.
-	//   - Logs to stdout.
-	//   - RFC3339 with UTC time format.
-	r.Use(ginzap.GinzapWithConfig(appLogger, &ginzap.Config{
-		UTC:        true,
-		TimeFormat: time.RFC3339,
-		Context: ginzap.Fn(func(c *gin.Context) []zapcore.Field {
-			fields := []zapcore.Field{}
-			// log request ID
-			if requestID := requestid.Get(c); requestID != "" {
-				fields = append(fields, zap.String("request_id", requestID))
-			}
-
-			// log trace and span ID
-			if trace.SpanFromContext(c.Request.Context()).SpanContext().IsValid() {
-				fields = append(fields, zap.String("trace_id", trace.SpanFromContext(c.Request.Context()).SpanContext().TraceID().String()))
-				fields = append(fields, zap.String("span_id", trace.SpanFromContext(c.Request.Context()).SpanContext().SpanID().String()))
-			}
-
-			// log request body
-			var body []byte
-			var buf bytes.Buffer
-			tee := io.TeeReader(c.Request.Body, &buf)
-			body, _ = io.ReadAll(tee)
-			c.Request.Body = io.NopCloser(&buf)
-			fields = append(fields, zap.String("body", string(body)))
-
-			return fields
-		}),
-		Skipper: func(c *gin.Context) bool {
-			return c.Request.URL.Path == "/healthz" && c.Request.Method == "GET"
-		},
-	}))
+	r.Use(addTraceMiddleware(appLogger))
+	r.Use(addTraceResponseMiddleware())
+	r.Use(logRequestMiddleware(appLogger))
 
 	// Logs all panic to error log
 	//   - stack means whether output the stack info.
@@ -149,7 +80,88 @@ func main() {
 	r.Run()
 }
 
+// Add a ginzap middleware, which:
+//   - Logs all requests, like a combined access and error log.
+//   - Logs to stdout.
+//   - RFC3339 with UTC time format.
+func logRequestMiddleware(appLogger *zap.Logger) gin.HandlerFunc {
+	return ginzap.GinzapWithConfig(appLogger, &ginzap.Config{
+		UTC:        true,
+		TimeFormat: time.RFC3339,
+		Context: ginzap.Fn(func(c *gin.Context) []zapcore.Field {
+			fields := []zapcore.Field{}
+			// log request ID
+			if requestID := requestid.Get(c); requestID != "" {
+				fields = append(fields, zap.String("request_id", requestID))
+			}
+
+			// log trace and span ID
+			if trace.SpanFromContext(c.Request.Context()).SpanContext().IsValid() {
+				fields = append(fields, zap.String("trace_id", trace.SpanFromContext(c.Request.Context()).SpanContext().TraceID().String()))
+				fields = append(fields, zap.String("span_id", trace.SpanFromContext(c.Request.Context()).SpanContext().SpanID().String()))
+			}
+
+			// log request body
+			var body []byte
+			var buf bytes.Buffer
+			tee := io.TeeReader(c.Request.Body, &buf)
+			body, _ = io.ReadAll(tee)
+			c.Request.Body = io.NopCloser(&buf)
+			fields = append(fields, zap.String("body", string(body)))
+
+			return fields
+		}),
+		Skipper: func(c *gin.Context) bool {
+			return c.Request.URL.Path == "/healthz" && c.Request.Method == "GET"
+		},
+	})
+}
+
+// Middleware to set tracing headers in the response
+// - X-Request-Id
+// - traceparent
+// - Server-Timing
+func addTraceResponseMiddleware() func(c *gin.Context) {
+	return func(c *gin.Context) {
+		// X-Request-ID
+		if rid := requestid.Get(c); rid != "" {
+			c.Header("X-Request-Id", rid)
+		}
+
+		// traceparent + Server-Timing
+		sc := trace.SpanFromContext(c.Request.Context()).SpanContext()
+		if sc.IsValid() {
+			// W3C traceparent
+			tp := "00-" + sc.TraceID().String() + "-" + sc.SpanID().String() + "-01"
+			c.Header("traceparent", tp)
+			c.Header("Server-Timing", `traceparent;desc="`+tp+`"`)
+		}
+		c.Next()
+	}
+}
+
+func addTraceMiddleware(appLogger *zap.Logger) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		// Add request ID to logger
+		reqID := requestid.Get(c)
+		appLogger = appLogger.With(zap.String("request_id", reqID))
+
+		// Add trace_id and span_id to logger
+		sc := trace.SpanFromContext(c.Request.Context()).SpanContext()
+		if sc.IsValid() {
+			appLogger = appLogger.With(
+				zap.String("trace_id", sc.TraceID().String()),
+				zap.String("span_id", sc.SpanID().String()),
+			)
+		}
+		c.Set("logger", appLogger.With(zap.String("request_id", reqID)))
+		c.Next()
+	}
+}
+
 func initOTel() func(context.Context) error {
+	// TODO : configure OTLP exporter to send data to Collector
+	// For demo, we use stdout exporter
 	// 1) Exporter: use stdout for demo; for production prefer OTLP -> Collector
 	exp, err := stdouttrace.New(stdouttrace.WithPrettyPrint())
 	if err != nil {
